a2fe28a6a824dc684a85cb142a2139d08f25dca1
Randall Dunn <justthisguyrandall@gmail.com>
2015-03-31 14:48:59 -0700

    Added function to correctly print lines of each letter block in series. Need to complete recusive functionn to put together the correct number of print lines

--- marquee.py
+++ marquee.py
@@ -25,7 +25,16 @@ lex = {'a': ("""
     0---
     0---
     -000
-    ----""")
+    ----"""),
+    '1': ("""
+    1111
+    2222
+    3333
+    4444
+    5555
+    6666
+    7777
+    """)
     }
 
 class Char(object):
@@ -33,20 +42,58 @@ class Char(object):
         self.char = char
 
     def disp_char(self):
-        print lex[self.char]
+        #print lex[self.char]
+        print 'Printing char\n'
+        char_pr = lex[self.char]
+        print len(char_pr)
+        print char_pr[4:9]
+        print lex[self.char][4:9]
         #for line in lex[self.char]
         #    print line
+
+    def get_char_line(self, x, y):
+        """Take the string index range and return the 'line'"""
+        return lex[self.char][x:y]
 
 class Msg(object):
     def __init__(self, *args):
         self.char_set = []
+        self.msg_length = len(args)
         for i in range(len(args)):
             temp = Char(args[i])
             self.char_set.append(temp)
 
+        # allLines should be a list of 7, each item containing all the x lines of each letter
+        self.allLines = []
+        i  = 0
+        x = 5
+        y = 9
+
+        while i < 7:
+            line = []
+            for value in range(self.msg_length):
+                item = self.char_set[value].get_char_line(x, y)
+                line.append(item+'-')
+            self.allLines.append(line)
+            i += 1
+            x += 9
+            y += 9
+
     def print_msg(self):
-        for i in range(len(self.char_set)):
-            self.char_set[i].disp_char()
+        #for i in range(self.msg_length):
+        #    # This refs a Char object
+        #    self.char_set[i].disp_char()
+        
+        for i in range(len(self.allLines)):
+            print 'len of msg_length\n'
+            print self.msg_length
+            value = 0
+            while value < self.msg_length:
+                print_statement = self.allLines[i][%d]' % value
+            print self.allLines[i][0]+self.allLines[i][1]+self.allLines[i][2]
+
+
+print 'Printing msg\n'
 msg1 = Msg('a', 'b', 'c')
 msg1.print_msg()
 
